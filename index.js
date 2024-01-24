133a2cd8f83e7a27f4fe3c0c721cd09a518489e7
Mukul <128701684+mukul-km@users.noreply.github.com>
2024-01-24 18:08:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,55 +6,58 @@ const convertInUnit = document.getElementById("convertInUnit");
 
 let temp;
 
+function checkFixed(temp){
+    temp = String(temp);
+    if(temp.indexOf(".") == -1){
+        return Number(temp);
+    }
+    else{
+        temp = Number(temp);
+        return temp.toFixed(2);
+    }
+}
+
 function convertTemperature(){
     temp = Number(inputTemperature.value);
     if(isNaN(temp)){
         window.alert("Enter a valid temperature!");
     }
     else if(temperatureUnit.value == convertInUnit.value){
-        if(temp == 0){
-            result.textContent = temp;
-            resultUnit.textContent = convertInUnit.value;  
-        }
-        else{
-            result.textContent = temp.toFixed(2);
-            resultUnit.textContent = convertInUnit.value;
-        }
-        
+        result.textContent = checkFixed(temp);
+        resultUnit.textContent = convertInUnit.value;  
     }
     else{
         if(temperatureUnit.value == "°C"){
             if(convertInUnit.value == "°F"){
-                result.textContent = ((temp * 9/5) + 32).toFixed(2);
+                result.textContent = checkFixed((temp * 9/5) + 32);
                 resultUnit.textContent = convertInUnit.value;
             }
             else{
-                result.textContent = (temp + 273.15).toFixed(2);
+                result.textContent = checkFixed(temp + 273.15);
                 resultUnit.textContent = convertInUnit.value;
             }
         }
         else if(temperatureUnit.value == "°F"){
             if(convertInUnit.value == "°C"){
-                result.textContent = ((temp - 32) * (5/9)).toFixed(2);
+                result.textContent = checkFixed((temp - 32) * (5/9));
                 resultUnit.textContent = convertInUnit.value;
             }
             else{
-                result.textContent = (((temp - 32) * (5/9)) + 273.15).toFixed(2);
+                result.textContent = checkFixed(((temp - 32) * (5/9)) + 273.15);
                 resultUnit.textContent = convertInUnit.value;
             }
         }
         else{
             if(convertInUnit.value == "°C"){
-                result.textContent = (temp - 273.15).toFixed(2);
+                result.textContent = checkFixed(temp - 273.15);
                 resultUnit.textContent = convertInUnit.value;
             }
             else{
-                result.textContent = (((temp - 273.15) * (9/5)) + 32).toFixed(2);
+                result.textContent = checkFixed(((temp - 273.15) * (9/5)) + 32);
                 resultUnit.textContent = convertInUnit.value;
             }
 
         }
     }
-
 
 }
